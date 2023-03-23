52f1a4eb822ebed1a85758377ac151549ebcef76
J-lng <jlungu.higgins@gmail.com>
2023-03-23 13:08:02 +0000

    delete-controller

--- controllers/items.js
+++ controllers/items.js
@@ -1,25 +1,6 @@
-import Item from '../models/items.js'
-
-
-
-// Delete route 
-export const deleteItem = async (req, res) => {
-  try {
-    const { id } = req.params
-    const deletedItem = await Item.findById(id)
-    if (!deletedItem) {
-      throw new Error('Item not found')
-    }
-    await Item.findByIdAndDelete(id)
-    return res.status(200).json({ message: 'Item deleted' })
-
-  } catch (err) {
-    return sendError(err, res)
-  }
-}
-=======
 import { sendError } from '../config/errors.js'
 import Item from '../models/items.js'
+
 
 //* INDEX route
 // endpoint: /items
@@ -43,5 +24,22 @@ export const createItem = async (req, res) => {
   } catch (err) {
     sendError(err, res)
   }
->>>>>>> bfe4c3316e2573edea1c65409771834cc26a7a05
+
+}
+
+
+// Delete route 
+export const deleteItem = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deletedItem = await Item.findById(id)
+    if (!deletedItem) {
+      throw new Error('Item not found')
+    }
+    await Item.findByIdAndDelete(id)
+    return res.status(200).json({ message: 'Item deleted' })
+
+  } catch (err) {
+    return sendError(err, res)
+  }
 }
