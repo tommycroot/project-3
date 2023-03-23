07ef6b74cf6fb95e192b185f4598d76360e5c022
Simon Davis <s.davis@hotmail.co.nz>
2023-03-23 16:53:25 +0000

    user updates

--- models/users.js
+++ models/users.js
@@ -15,8 +15,8 @@ const userSchema = new mongoose.Schema({
   location: { type: String, required: true, maxlength: 30 },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  items: { type: mongoose.Schema.ObjectId, ref: 'Item', required: true },
-  ratings : [sellerRatingsSchema]
+  items: [{ type: mongoose.Schema.ObjectId, ref: 'Item', required: true }],
+  ratings: [sellerRatingsSchema],
 })
 
 
@@ -27,19 +27,19 @@ userSchema.virtual('averageRating')
     if (!this.ratings.length) return 'No ratings for the user'
     
     const sum = this.ratings.reduce((acc, rating) => {
-      return acc + rating.rating;
+      return acc + rating.rating
     }, 0)
     
     return parseFloat((sum / this.ratings.length).toFixed(2))
   })
 
 
-// validation below 
-userSchema.virtual('items', {
-  ref: 'Item',
-  localField: '_id',
-  foreignField: 'owner',
-})
+// // validation below 
+// userSchema.virtual('items', {
+//   ref: 'Item',
+//   localField: '_id',
+//   foreignField: 'owner',
+// })
 
 
 userSchema.set('toJSON', {
