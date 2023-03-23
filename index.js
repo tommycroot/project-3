997e4103b1ecf5027023589168edac4fea83c08a
Simon Davis <s.davis@hotmail.co.nz>
2023-03-23 09:37:47 +0000

    working index.js without router

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+//!Variables
+
+const app = express()
+
+const startServer = async () => {
+  try {
+
+    //?Connect to mongoDB database
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('💎 Database connected')
+
+    //? Parse JSON to req.body
+    app.use(express.json())
+
+    //? Middleware
+
+    //Logger
+    app.use((req, res, next) => {
+      console.log(`☕️ Incoming request ${req.method} from ${req.url}`)
+      next()
+    })
+
+    //Router goes here
+    //app.use(router)
+
+    //? 404 catch all middleware
+    app.use((req, res) => {
+      return res.status(404).json({ message: 'Route does not exist.' })
+    })
+
+    //? Starting node server after connecting to database
+    app.listen(process.env.PORT, () => console.log(`🏆 Server up and running on port ${process.env.PORT}.`))
+
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+startServer()
