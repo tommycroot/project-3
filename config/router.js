268b32d280898bf0bec726230e44e2422cad39d1
Simon Davis <s.davis@hotmail.co.nz>
2023-03-24 11:36:50 +0000

    fixing git stuff

--- config/router.js
+++ config/router.js
@@ -2,10 +2,10 @@ import express from 'express'
 const router = express.Router()
 import { getItems, createItem, deleteItem, updateItem, getSingleItem } from '../controllers/items.js'
 import { addRating, deleteRating } from '../controllers/ratings.js'
-// import { addMessage, updateMessage, deleteMessage } from '../controllers/messages.js'
+import { addMessage, deleteMessage } from '../controllers/messages.js'
 import { registerUser, loginUser } from '../controllers/auth.js'
 import { secureRoute } from './secureRoute.js'
-//import { profileView, getSingleProfile } from '../controllers/users.js'
+import { profileView, getSingleProfile } from '../controllers/profiles.js'
 
 router.route('/items')
   .get(getItems)
@@ -28,20 +28,16 @@ router.route('/users/:userId/ratings')
 router.route('/users/:userId/ratings/:ratingId')
   .delete(secureRoute, deleteRating)
 
-// router.route('/items/:itemId/messages')
-//   .post(secureRoute, addMessage)
+router.route('/items/:itemId/messages')
+  .post(secureRoute, addMessage)
 
-// router.route('/items/:itemId/messages/:messageId')
-//   .put(secureRoute, updateMessage)
-//   .delete(secureRoute, deleteMessage)
+router.route('/items/:itemId/messages/:messageId')
+  .delete(secureRoute, deleteMessage)
 
-// router.route('/profile')
-//   .get(secureRoute, profileView)
+router.route('/profile')
+  .get(secureRoute, profileView)
 
-// router.route('/profile')
-//   .get(secureRoute, profileView)  
-
-// router.route('/profile/:profileId')
-//   .get(secureRoute, getSingleProfile)
+router.route('/profile/:profileId')
+  .get(secureRoute, getSingleProfile)
 
 export default router
